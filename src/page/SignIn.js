b0d7f24c7d0df4475b3885de8e2bb3c179c95175
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-16 13:53:07 -0500

    login

--- src/page/SignIn.js
+++ src/page/SignIn.js
@@ -1,9 +1,34 @@
 
+import { useState } from "react";
+import "../style/css/sign-in/signin.css";
 
-function SignIn() {
+function LogIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleEmail = (e) => {
+    setEmail(e)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e)
+  }
+
   return (
-    <div>SignIn</div>
+    <form className="signin">
+      <input 
+        className="signin-input" 
+        placeholder="Type your email" 
+        onChange={handleEmail} />
+      <input 
+        className="signin-input" 
+        placeholder="Type your password" 
+        onChange={handlePassword} />
+      <button className="signin-submit" type="submit">
+        SIGN IN
+      </button>
+    </form>
   )
-}
+} 
 
-export default SignIn
+export default LogIn
