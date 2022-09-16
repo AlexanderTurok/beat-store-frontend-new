c41e1ed7d5f279650b8d8411454301dbe07ea574
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-15 21:30:22 -0500

    change to li

--- src/component/music-form/Columns.js
+++ src/component/music-form/Columns.js
@@ -4,13 +4,13 @@ import "../../style/css/music-form/music-columns.css"
 
 function Columns() {
   return (
-    <div className="columns">
+    <li className="columns">
       <strong className='column title'> TITLE </strong>
       <strong className='column bpm'> BPM </strong>
       <strong className='column key'> KEY </strong>
       <strong className='column tags'> TAGS </strong>
       <strong className='column options'> OPTIONS </strong>
-    </div>
+    </li>
   )
 }
 
